1e50824d171d020aa9bff0e0b902a9e748ecfb6a
Zack Barbari <zackb@unc.edu>
2023-06-13 15:20:24 -0400

    async phaser import (next is goofy)

--- app/game.tsx
+++ app/game.tsx
@@ -1,3 +1,32 @@
-import React, { useState, useEffect } from 'react';
+'use client';
 
-import { Game as GameType } from 'phaser'
+import React, { useState, useEffect } from 'react';
+import { Game as GameType } from 'phaser';
+
+const Game = () => {
+  const [game, setGame] = useState<GameType>();
+
+    useEffect(() => {
+        async function initPhaser() {
+            const Phaser = await import('phaser');
+
+            const trafficGame = new Phaser.Game({
+                type: Phaser.AUTO,
+                title: 'Das Traffic Gamenheimer',
+                parent: 'game-content',
+                width: window.innerWidth,
+                height: window.innerHeight,
+                scene: {
+
+                }
+            })
+        }
+        initPhaser();
+    }, []);
+
+    return (
+        <>
+            <div id='game-content' key='game-content'></div>
+        </>
+    )
+}
